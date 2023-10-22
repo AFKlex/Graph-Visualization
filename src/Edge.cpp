92315410bf5c8f5064b7b23b1a03bd83a9079a4b
Alexander Agricola <agricolaalex@gmail.com>
2023-10-22 17:00:28 +0200

    Add edge class for connection between nodes.

---
+++ src/Edge.cpp
@@ -0,0 +1,9 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#include "header/Edge.h"
+Edge::Edge(Node* first, Node* second){
+    this->firstNode = first;
+    this->secondNode = second;
+}
---
+++ src/header/Edge.h
@@ -0,0 +1,16 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#ifndef GRAPH_VISUALIZATION_EDGE_H
+#define GRAPH_VISUALIZATION_EDGE_H
+
+#include "header/Node.h"
+class Edge {
+    Node* firstNode;
+    Node* secondNode;
+    Edge(Node* first ,Node* second);
+};
+
+
+#endif //GRAPH_VISUALIZATION_EDGE_H
