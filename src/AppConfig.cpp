9d351cc8b321a9a66a9af2bbf415ae922e9c1b51
Alexander Agricola <agricolaalex@gmail.com>
2023-10-22 16:54:50 +0200

    move often used variables to AppConfig.cpp

--- src/AppConfig.cpp
+++ src/AppConfig.cpp
@@ -4,6 +4,14 @@
 
 #include "header/AppConfig.h"
 namespace AppConfig{
+    SDL_Window * WINDOW = nullptr;
+    SDL_Surface *SCREEN_SURFACE = nullptr;
+    SDL_Renderer *RENDERER = nullptr;
+
     const int WINDOW_WIDTH = 800;
     const int WINDOW_HEIGHT = 800;
+    const SDL_Color TEXT_COLOR = {255,0,0};
+    TTF_Font* FONT = nullptr;
+
+    SDL_Texture *textTexture = nullptr;
 }
--- src/header/AppConfig.h
+++ src/header/AppConfig.h
@@ -5,10 +5,22 @@
 #ifndef GRAPH_VISUALIZATION_APPCONFIG_H
 #define GRAPH_VISUALIZATION_APPCONFIG_H
 
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
 
 namespace AppConfig{
+    extern SDL_Window * WINDOW;
+    extern SDL_Surface *SCREEN_SURFACE;
+    extern SDL_Renderer *RENDERER;
+
     extern  const int WINDOW_WIDTH;
     extern const int WINDOW_HEIGHT;
+
+    extern const SDL_Color TEXT_COLOR;
+    extern TTF_Font* FONT;
+    extern SDL_Texture *textTexture;
+
+
 }
 
 
