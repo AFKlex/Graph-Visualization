c1e83bce6944a1c1a6f53837917d7db52f649bba
Alexander Agricola <agricolaalex@gmail.com>
2023-10-22 11:56:02 +0200

    Add Global Configuration

---
+++ src/AppConfig.cpp
@@ -0,0 +1,9 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#include "header/AppConfig.h"
+namespace AppConfig{
+    const int WINDOW_WIDTH = 800;
+    const int WINDOW_HEIGHT = 800;
+}
---
+++ src/header/AppConfig.h
@@ -0,0 +1,15 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#ifndef GRAPH_VISUALIZATION_APPCONFIG_H
+#define GRAPH_VISUALIZATION_APPCONFIG_H
+
+
+namespace AppConfig{
+    extern  const int WINDOW_WIDTH;
+    extern const int WINDOW_HEIGHT;
+}
+
+
+#endif //GRAPH_VISUALIZATION_APPCONFIG_H
