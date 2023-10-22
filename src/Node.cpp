86f7cd9f4ba3934c62afe641b13624e0df9a5210
Alexander Agricola <agricolaalex@gmail.com>
2023-10-22 15:57:54 +0200

    Add initial information for Node.

---
+++ src/Node.cpp
@@ -0,0 +1,11 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#include "header/Node.h"
+Node::Node(std::string name, int x, int y){
+    this->x = x;
+    this->y = y;
+    this->name = name;
+}
+
---
+++ src/header/Node.h
@@ -0,0 +1,17 @@
+//
+// Created by afklex on 10/22/23.
+//
+
+#ifndef GRAPH_VISUALIZATION_NODE_H
+#define GRAPH_VISUALIZATION_NODE_H
+#include<iostream>
+
+class Node {
+public:
+    std::string name;
+    int x,y;
+    Node(std::string name, int x, int y);
+};
+
+
+#endif //GRAPH_VISUALIZATION_NODE_H
