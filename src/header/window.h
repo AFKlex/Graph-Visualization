a1cf3b82c8775469ba70592727a2d9374104f7b3
Alexander Agricola <agricolaalex@gmail.com>
2023-10-22 16:12:25 +0200

    Implement SDL2_ttf

--- src/header/window.h
+++ src/header/window.h
@@ -2,9 +2,11 @@
 // Created by afklex on 10/22/23.
 //
 #include<SDL2/SDL.h>
+#include "header/Node.h"
+#include<SDL2/SDL_ttf.h>
 #ifndef GRAPH_VISUALIZATION_WINDOW_H
 #define GRAPH_VISUALIZATION_WINDOW_H
 bool initGame(SDL_Window *&ptrWindow, SDL_Surface *&ptrSurface, SDL_Renderer *&ptrRenderer);
 void destroyGame(SDL_Window *&ptrWindow, SDL_Surface *&ptrSurface, SDL_Renderer *&ptrRenderer);
-
+void drawNode(SDL_Renderer *& ptrRender, Node node);
 #endif //GRAPH_VISUALIZATION_WINDOW_H
--- src/window.cpp
+++ src/window.cpp
@@ -4,6 +4,7 @@
 #include<SDL2/SDL.h>
 #include<iostream>
 #include "header/AppConfig.h"
+#include"header/window.h"
 bool initWindow(SDL_Window *&ptrWindow){
     bool  success = true;
 
@@ -13,6 +14,21 @@ bool initWindow(SDL_Window *&ptrWindow){
         std::cout << "Window Could not be loaded! SDL Error: " << SDL_GetError() << std::endl;
         success = false;
     }
+    return success;
+}
+
+bool initFont(SDL_Surface *&ptrMessage, TTF_Font *&ptrFont){
+    bool  success = true;
+    if( TTF_Init() == -1 ){ //Initialize SDL_ttf
+        std::cout << "Failed to load Font" << std::endl;
+        success = false;
+    }
+    ptrFont = TTF_OpenFont("../assets/Gothic.ttf", 100);
+    if(ptrFont == nullptr){
+        std::cout << "Failed to load Font! TTF_Error: " << TTF_GetError() << std::endl;
+        success = false;
+    }
+
     return success;
 }
 
@@ -46,6 +62,57 @@ bool initGame(SDL_Window *&ptrWindow, SDL_Surface *&ptrSurface, SDL_Renderer *&p
     }
 
     return success;
+}
+
+int
+SDL_RenderDrawCircle(SDL_Renderer * renderer, int x, int y, int radius)
+{
+    int offsetx, offsety, d;
+    int status;
+
+    //CHECK_RENDERER_MAGIC(renderer, -1);
+
+    offsetx = 0;
+    offsety = radius;
+    d = radius -1;
+    status = 0;
+
+    while (offsety >= offsetx) {
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
+
+        if (status < 0) {
+            status = -1;
+            break;
+        }
+
+        if (d >= 2*offsetx) {
+            d -= 2*offsetx + 1;
+            offsetx +=1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
+
+    return status;
+}
+
+void drawNode(SDL_Renderer *& ptrRender, Node node){
+    SDL_SetRenderDrawColor(ptrRender,255,0,0,0);
+    SDL_RenderDrawCircle(ptrRender, node.x, node.y,15);
 }
 
 void destroyGame(SDL_Window *&ptrWindow, SDL_Surface *&ptrSurface, SDL_Renderer *&ptrRenderer){
