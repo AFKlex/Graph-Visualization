84c28fb3ea02ee6e9a090b2d247cdf101736816b
Alexander Agricola <agricolaalex@gmail.com>
2023-10-23 16:54:25 +0200

    Add function to create node between edges.

--- src/Edge.cpp
+++ src/Edge.cpp
@@ -7,3 +7,24 @@ Edge::Edge(Node* first, Node* second){
     this->A = first;
     this->B = second;
 }
+
+void createEdgeBetweenNodes(int x, int y, Node *firstNode, Node *secondNode, std::vector<Edge> edgeVector ,std::vector<Node> nodeVector ,bool oneNodeSelected){
+    // Connect two nodes
+    Node *selection = checkNodeExistAtPosition(&nodeVector,x, y);
+
+    if(oneNodeSelected && selection != nullptr){
+        secondNode = selection;
+        edgeVector.emplace_back(firstNode, secondNode);
+        oneNodeSelected = false;
+        firstNode = nullptr;
+        secondNode = nullptr;
+
+    }else if(selection != nullptr){
+        oneNodeSelected  = true;
+        firstNode = selection;
+    }else{
+        std::cout << "No node was picked!" << std::endl;
+    }
+
+
+}
